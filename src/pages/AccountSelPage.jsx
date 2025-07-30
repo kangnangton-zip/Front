4ee1837f8663cbe801dc93a6ce3f56be06518e0f
roko02 <90667780+ROKO02@users.noreply.github.com>
2025-07-30 19:47:30 +0900

    update AccountSelectPage - jong
    계정선택페이지 업데이트 및 브라우저 아이콘 및 이름 추가

--- src/pages/AccountSelPage.jsx
+++ src/pages/AccountSelPage.jsx
@@ -1,3 +1,4 @@
+import AccountSelect from "../components/AccountSelect";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 
@@ -5,10 +6,7 @@ function AccountSelPage() {
   return (
     <>
       <Header />
-      <div className="account_selection">
-        <h1>계정 선택</h1>
-        <p>사용할 계정을 선택하세요.</p>
-      </div>
+      <AccountSelect />
       <Footer />
     </>
   );
