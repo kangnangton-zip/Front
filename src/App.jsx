e83056546fd14a8a7fc0b3eda709d92012b07d7e
roko02 <90667780+ROKO02@users.noreply.github.com>
2025-07-30 12:38:52 +0900

    create AccountSelectPage - jong
    계정선택페이지 제작 시작

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import AccountSelPage from "./pages/AccountSelPage";
 import MainPage from "./pages/MainPage";
 
 function App() {
   return (
     <>
-      <MainPage />
+      {/* <MainPage /> */}
+      <AccountSelPage />
     </>
   );
 }
---
+++ src/pages/AccountSelPage.jsx
@@ -0,0 +1,17 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+function AccountSelPage() {
+  return (
+    <>
+      <Header />
+      <div className="account_selection">
+        <h1>계정 선택</h1>
+        <p>사용할 계정을 선택하세요.</p>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default AccountSelPage;
